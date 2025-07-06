8008e05290567d4077471d96cae0fd2b70fcb80b
Abdul1013 <abdulhaqabdulrasheed@gmail.com>
2025-07-06 06:23:35 +0100

    edited folder structure - components

--- frontend/components/theme/normalize.js
+++
@@ -1,6 +0,0 @@
-// theme/normalize.js
-// import { scale, verticalScale, moderateScale } from 'react-native-size-matters';
-import{scale,moderateScale,verticalScale} from 'react-native-size-matters'
-export const normalizeFont = (size) => moderateScale(size);
-export const normalizeSpace = (size) => scale(size);
-export const normalizeHeight = (size) => verticalScale(size);
