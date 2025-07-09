93d7410527b09411ad1691c7063b9a1aa3b33112
Abdul1013 <abdulhaqabdulrasheed@gmail.com>
2025-07-09 17:02:49 +0100

    created App content for App.jsx

---
+++ frontend/App/components/AppContent.jsx
@@ -0,0 +1,57 @@
+// AppContent.js
+import React, { useEffect } from "react";
+import { useAuth } from "../hooks/authContext";
+import { useFonts } from "expo-font";
+import * as SplashScreen from "expo-splash-screen";
+import { ActivityIndicator, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import * as Linking from "expo-linking";
+
+import RootNavigator from "../navigation/RootNavigator";
+import AuthNavigator from "../navigation/AuthNavigator";
+
+SplashScreen.preventAutoHideAsync();
+
+const linking = {
+  prefixes: ["janco://"],
+  config: {
+    screens: {
+      AuthCallback: "auth/callback",
+      Home: "home",
+      UpdatePassword: "reset-password",
+    },
+  },
+};
+
+export default function AppContent() {
+  const { user, loading } = useAuth();
+  const [fontsLoaded] = useFonts({
+    Chewy: require("../../assets/fonts/Chewy-Regular.ttf"),
+    Quicksand: require("../../assets/fonts/Quicksand-Regular.ttf"),
+    QuicksandMedium: require("../../assets/fonts/Quicksand-Medium.ttf"),
+    QuicksandBold: require("../../assets/fonts/Quicksand-Bold.ttf"),
+  });
+
+  useEffect(() => {
+    const hide = async () => {
+      if (fontsLoaded && !loading) {
+        await SplashScreen.hideAsync();
+      }
+    };
+    hide();
+  }, [fontsLoaded, loading]);
+
+  if (!fontsLoaded || loading) {
+    return (
+      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+        <ActivityIndicator size="large" />
+      </View>
+    );
+  }
+
+  return (
+    <NavigationContainer linking={linking}>
+      {user ? <RootNavigator /> : <AuthNavigator />}
+    </NavigationContainer>
+  );
+}
