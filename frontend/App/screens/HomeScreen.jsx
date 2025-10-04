b3ac12fe642252f1648ff4089bbfc6dcd4e91e09
Abdul1013 <abdulhaqabdulrasheed@gmail.com>
2025-10-04 08:48:56 +0100

    implemented dynamic dashboard

--- frontend/App/screens/HomeScreen.jsx
+++ frontend/App/screens/HomeScreen.jsx
@@ -4,13 +4,12 @@ import {
   Text,
   StyleSheet,
   TouchableOpacity,
-  FlatList,
-  Image,
+  ScrollView,
 } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import Icon from "react-native-vector-icons/FontAwesome5";
 import MaterialIcons from "@expo/vector-icons/MaterialIcons";
-import {Colors, Typography, Spacing} from "../components/theme/Theme";
+import { Colors, Typography, Spacing } from "../components/theme/Theme";
 import { useNavigation } from "@react-navigation/native";
 import { Octicons } from "@expo/vector-icons";
 import { useAuth } from "../hooks/authContext";
@@ -34,7 +33,7 @@ const serviceCategories = [
 
 export default function HomeScreen() {
   const navigation = useNavigation();
-const {user,profile, loading} = useAuth();
+  const { user, profile, loading } = useAuth();
 
   return (
     <SafeAreaView style={Typography.container}>
@@ -48,67 +47,76 @@ const {user,profile, loading} = useAuth();
       >
         <View>
           <Text style={Typography.title}>Welcome, {profile?.user_name}</Text>
-        
         </View>
         <TouchableOpacity onPress={() => navigation.navigate("Notifications")}>
           <MaterialIcons name="notifications" size={24} color="white" />
         </TouchableOpacity>
       </View>
-          <Text
-            style={[
-              Typography.note,
-              { color: Colors.light.textMuted, marginBottom: 15 },
-            ]}
-          >
-           {profile?.address}
-          </Text>
-
-      {/* <Icon name="alert"/> */}
-        <Text style={[Typography.subtitle , {textAlign:'center', marginBottom: Spacing.md, }]}>What Chore?</Text>
-      <View style={styles.gridContainer}>
-        {serviceCategories.map((item, index) => (
-          <TouchableOpacity
-            key={index}
-            style={styles.gridCard}
-            activeOpacity={0.8}
-            onPress={() =>
-              navigation.navigate("ServiceBooking", { serviceType: item.title })
-            }
-          >
-            <Icon name={item.icon} size={24} color="#2D2D2D" />
-            <Text style={styles.cardText}>{item.title}</Text>
-            {item.com && <Text style={styles.comingSoon}>{item.com}</Text>}
-          </TouchableOpacity>
-        ))}
-      </View>
-
-      <View style={styles.columnCard}>
-        <Text style={Typography.subtitle}>🗓 Your next booking is on</Text>
-        <Text style={[Typography.default, { color: Colors.light.textMuted }]}>
-          Thursday, June 27 - 5:00 PM
+      <Text
+        style={[
+          Typography.note,
+          { color: Colors.light.textMuted, marginBottom: 15 },
+        ]}
+      >
+        {profile?.address}
+      </Text>
+      <ScrollView contentContainerStyle={{marginBottom:0 }} contentInsetAdjustmentBehavior="never" showsVerticalScrollIndicator={false}>
+        {/* <Icon name="alert"/> */}
+        <Text
+          style={[
+            Typography.subtitle,
+            { textAlign: "center", marginBottom: Spacing.md },
+          ]}
+        >
+          What Chore?
         </Text>
-        <TouchableOpacity style={styles.bookingButton}>
-          <Text style={styles.bookingText}>Book A Service</Text>
-        </TouchableOpacity>
-      </View>
+        <View style={styles.gridContainer}>
+          {serviceCategories.map((item, index) => (
+            <TouchableOpacity
+              key={index}
+              style={styles.gridCard}
+              activeOpacity={0.8}
+              onPress={() =>
+                navigation.navigate("ServiceBooking", {
+                  serviceType: item.title,
+                })
+              }
+            >
+              <Icon name={item.icon} size={24} color="#2D2D2D" />
+              <Text style={styles.cardText}>{item.title}</Text>
+              {item.com && <Text style={styles.comingSoon}>{item.com}</Text>}
+            </TouchableOpacity>
+          ))}
+        </View>
 
-      <View style={styles.gridContainer}>
-        <TouchableOpacity
-          style={styles.gridCard}
-          onPress={() => navigation.navigate("PricingEstimate")}
-        >
-          <Octicons name="history" size={24} color="black" />
-          <Text style={styles.gridText}>Summaries</Text>
-        </TouchableOpacity>
+        <View style={styles.columnCard}>
+          <Text style={Typography.subtitle}>🗓 Your next booking is on</Text>
+          <Text style={[Typography.default, { color: Colors.light.textMuted }]}>
+            Thursday, June 27 - 5:00 PM
+          </Text>
+          <TouchableOpacity style={styles.bookingButton}>
+            <Text style={styles.bookingText}>Book A Service</Text>
+          </TouchableOpacity>
+        </View>
 
-        <TouchableOpacity
-          style={styles.gridCard}
-          onPress={() => navigation.navigate("Chat")}
-        >
-          <Icon name="comments" size={22} color="#000" />
-          <Text style={styles.gridText}>Talk to Janitor</Text>
-        </TouchableOpacity>
-      </View>
+        <View style={styles.gridContainer}>
+          <TouchableOpacity
+            style={styles.gridCard}
+            onPress={() => navigation.navigate("PricingEstimate")}
+          >
+            <Octicons name="history" size={24} color="black" />
+            <Text style={styles.gridText}>Summaries</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity
+            style={styles.gridCard}
+            onPress={() => navigation.navigate("Chat")}
+          >
+            <Icon name="comments" size={22} color="#000" />
+            <Text style={styles.gridText}>Talk to Janitor</Text>
+          </TouchableOpacity>
+        </View>
+      </ScrollView>
     </SafeAreaView>
   );
 }
@@ -124,7 +132,7 @@ const styles = StyleSheet.create({
     fontSize: 16,
     fontWeight: "600",
     color: Colors.light.text,
-    textAlign:'center'
+    textAlign: "center",
   },
   comingSoon: {
     fontSize: 12,
@@ -135,12 +143,12 @@ const styles = StyleSheet.create({
     flexDirection: "row",
     flexWrap: "wrap",
     justifyContent: "space-between",
-  
+    // gap: Spacing.md
   },
 
   gridCard: {
     backgroundColor: Colors.light.accent,
-    borderColor:Colors.dark.text,
+    borderColor: Colors.dark.text,
     borderWidth: 0.5,
     padding: 20,
     borderRadius: 10,
@@ -148,10 +156,9 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "center",
     width: "48%",
+    // flexBasis:"48%",
+    // flexGrow: 1,
     marginBottom: 12,
-
-
-
   },
   gridText: {
     marginTop: 10,
@@ -160,14 +167,13 @@ const styles = StyleSheet.create({
   },
   columnCard: {
     alignItems: "center",
-    justifyContent:"center",
+    justifyContent: "center",
     backgroundColor: Colors.dark.primary,
-    borderColor:Colors.dark.text,
+    borderColor: Colors.dark.text,
     borderWidth: 1,
     padding: 16,
     borderRadius: 10,
     marginBottom: 20,
-    
   },
   bookingButton: {
     borderColor: Colors.dark.shadow,
